ab6be13004b229aece231f194b7546e56794fe0d
Clara <clara.vatus@gmail.com>
2025-12-28 10:23:55 +0000

    ajout

--- dijkstra.h
+++ dijkstra.h
@@ -6,6 +6,11 @@
 #include "coord.h"
 #include "grille.h"
 
+float dijkstra(
+        grille_t grille,
+        coord_t source, coord_t destination,
+        float seuil,
+        liste_noeud_t** chemin);
 
 
 #endif
